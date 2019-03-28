8aae0c1c07873ce07ff3854c2451c0638313efd0
Debashis <jupiter_104@ymail.com>
2019-03-28 16:39:54 +0530

    Add files via upload

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type some numbers separated by comma like 4,8,2,12,9");
+
+            string input = Console.ReadLine();
+
+            int[] numbers = input.Split(',').Select(int.Parse).ToArray();
+
+            int maxNumber = numbers.Max();
+
+            Console.WriteLine("The largest number is " + maxNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
